ba9448405f685934326fb632c38c8995a5670438
Duy Lạc <lachongduy@gmail.com>
2021-10-17 21:07:47 +0700

    lachongduy

---
+++ style/cart.js
@@ -0,0 +1,12 @@
+var i = 0;
+    function buttonminus() {
+       
+
+        i--;
+        document.getElementById('inc').value = i;
+    }
+    var i = 0;
+    function buttonplus() {
+        i++;
+        document.getElementById('inc').value = i;
+    }
