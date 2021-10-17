6414d33436e35dc3a88fcb47f3c5715c13b48ba4
KhangNT1105 <KhangNT1105>
2021-10-17 22:32:49 +0700

    resolved conflict branch aonhattan and lachongduy

---
+++ js/cart.js
@@ -0,0 +1,10 @@
+var i = 0;
+function buttonminus() {
+  i--;
+  document.getElementById("inc").value = i;
+}
+var i = 0;
+function buttonplus() {
+  i++;
+  document.getElementById("inc").value = i;
+}
