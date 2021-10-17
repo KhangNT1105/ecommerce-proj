56fce9473d7a9342cd1aaa2165f61439fd18f338
KhangNT1105 <KhangNT1105>
2021-10-17 19:48:35 +0700

    refactor code home.js

--- js/home.js
+++ js/home.js
@@ -1,189 +1,86 @@
-$('.slick-slider').slick({
+$(".slick-slider").slick({
   dots: true,
-  autoplay:true
+  autoplay: true,
 });
-$('.boxhome__tille--slick-slider').slick({
-  autoplay:true
+$(".boxhome__tille--slick-slider").slick({
+  autoplay: true,
 });
-
-
 
 /* click model */
-        const receiveNews = document.getElementById('js-receive-news')
-        
-        const model = document.querySelector('.js-model')
-        const modelContainer = document.querySelector('.js-model-container')
-        const modelClose = document.querySelector('.js-model-close')
-        //show model
-        function showFormNews() {
-            model.classList.add('open')
-        }
-        //close model
-        function closeFormNews() {
-            model.classList.remove('open')
-        }
-        receiveNews.addEventListener('click', showFormNews)
-        //ngăn chặn hành vi mặt định của model
-        modelContainer.addEventListener('click', function (event) {
-          event.stopPropagation()
-        })
-        //khi click vào icon sẽ close model
-        modelClose.addEventListener('click', closeFormNews)
-        model.addEventListener('click', closeFormNews)
+const receiveNews = document.getElementById("js-receive-news");
 
+const model = document.querySelector(".js-model");
+const modelContainer = document.querySelector(".js-model-container");
+const modelClose = document.querySelector(".js-model-close");
+//show model
+function showFormNews() {
+  model.classList.add("open");
+}
+//close model
+function closeFormNews() {
+  model.classList.remove("open");
+}
+receiveNews.addEventListener("click", showFormNews);
+//ngăn chặn hành vi mặt định của model
+modelContainer.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
+//khi click vào icon sẽ close model
+modelClose.addEventListener("click", closeFormNews);
+model.addEventListener("click", closeFormNews);
 
-        //modal resposive
-        const jsModalTablet = document.querySelector('.js-receive-news-mobile')
-        jsModalTablet.addEventListener('click', showFormNews) 
+//modal resposive
+const jsModalTablet = document.querySelector(".js-receive-news-mobile");
+jsModalTablet.addEventListener("click", showFormNews);
 
 /* Menu hidden responsive */
-const menuHidden = document.getElementById('js-menu-hidden')
-const closeMenuHidden = document.querySelector('.js-close-menu-hidden ')
-const modalHidden = document.querySelector('.js-modal-menu')
-function showMenu(){
-  modalHidden.classList.add('active')
-  
+const menuHidden = document.getElementById("js-menu-hidden");
+const closeMenuHidden = document.querySelector(".js-close-menu-hidden ");
+const modalHidden = document.querySelector(".js-modal-menu");
+function showMenu() {
+  modalHidden.classList.add("active");
 }
-function closeMenu(){
-  modalHidden.classList.remove('active')
+function closeMenu() {
+  modalHidden.classList.remove("active");
 }
-menuHidden.addEventListener('click', showMenu)
-closeMenuHidden.addEventListener('click', closeMenu)
+menuHidden.addEventListener("click", showMenu);
+closeMenuHidden.addEventListener("click", closeMenu);
 
-/* Dropdown */  
-const iconHiddenMenu = document.querySelector('.js-menu-hidden-icon')     
-const listHiddenMenu = document.querySelector('.submenu-hidden__list')    
+const showSubMenu = (list) => (e) => {
+  list.classList.toggle("hidden");
+  list.classList.add("animate__animated", "animate__fadeInRight");
+};
 
+const listSubMenu = document.querySelectorAll(".js-menu");
 
-
-
-const iconHiddenMenuSubcoffee = document.querySelector('.js-menu-hidden-icon-coffee ') 
-const listHiddenMenucoffee = document.querySelector('.submenu-hidden__list--coffee')     
-
-
-
-const iconHiddenMenuSubFreeze = document.querySelector('.js-menu-hidden-icon-freeze') 
-const listHiddenMenuFreeze = document.querySelector('.submenu-hidden__list--freeze')    
-
-
-const iconHiddenMenuSubTea = document.querySelector('.js-menu-hidden-icon-tea') 
-const listHiddenMenuTea = document.querySelector('.submenu-hidden__list--tea')   
-
-
-const iconHiddenMenuSubbakery = document.querySelector('.js-menu-hidden-icon-bakery') 
-const listHiddenMenubakery = document.querySelector('.submenu-hidden__list--bakery')   
-
-
-const iconHiddenMenuSuborder = document.querySelector('.js-menu-hidden-icon-order') 
-const listHiddenMenuorder = document.querySelector('.submenu-hidden__list--order')   
-
-const iconHiddenMenuSubnews = document.querySelector('.js-menu-hidden-icon-news') 
-const listHiddenMenunews = document.querySelector('.submenu-hidden__list--news') 
-
-
-const iconHiddenMenuSubCommunity = document.querySelector('.js-menu-hidden-icon-community ') 
-const listHiddenMenuCommunity = document.querySelector('.submenu-hidden__list-community') 
-
-const iconHiddenMenuSubcultural = document.querySelector('.js-menu-hidden-icon-cultural ') 
-const listHiddenMenucultural = document.querySelector('.submenu-hidden__list--cultural') 
-
-const iconHiddenMenuSubcommunities = document.querySelector('.js-menu-hidden-icon-communities ') 
-const listHiddenMenucommunities = document.querySelector('.submenu-hidden__list--communities') 
-
-
-
-
-const iconHiddenMenuSubgobackme = document.querySelector('.js-menu-hidden-icon-gobackme') 
-const listHiddenMenugobackme = document.querySelector('.submenu-hidden__list--gobackme') 
-
-function showSubMenuHidden(){
-  listHiddenMenu.classList.toggle('hidden')
-  listHiddenMenu.classList.add('animate__animated', 'animate__fadeInRight'); 
-}
-
-
-function showSubMenuHiddenSubcoffee(){
-  listHiddenMenucoffee.classList.toggle('hidden')
-  listHiddenMenucoffee.classList.add('animate__animated', 'animate__fadeInRight');
-}
-
-
-function showSubMenuHiddenSubFreeze(){
-  listHiddenMenuFreeze.classList.toggle('hidden')
-  listHiddenMenuFreeze.classList.add('animate__animated', 'animate__fadeInRight');
-}
-
-function showSubMenuHiddenSubTea(){
-  listHiddenMenuTea.classList.toggle('hidden')
-  listHiddenMenuTea.classList.add('animate__animated', 'animate__fadeInRight');
-}
-function showSubMenuHiddenSubBakery(){
-  listHiddenMenubakery.classList.toggle('hidden')
-  listHiddenMenubakery.classList.add('animate__animated', 'animate__fadeInRight');
-}
-function showSubMenuHiddenSuborder(){
-  listHiddenMenuorder.classList.toggle('hidden')
-  listHiddenMenuorder.classList.add('animate__animated', 'animate__fadeInRight');
-}
-function showSubMenuHiddenSubnews(){
-  listHiddenMenunews.classList.toggle('hidden')
-  listHiddenMenunews.classList.add('animate__animated', 'animate__fadeInRight');
-}
-function showSubMenuHiddenSubgobackme(){
-  listHiddenMenugobackme.classList.toggle('hidden')
-  listHiddenMenugobackme.classList.add('animate__animated', 'animate__fadeInRight');
-}
-function showSubMenuHiddenSubCommunity(){
-  listHiddenMenuCommunity.classList.toggle('hidden')
-  listHiddenMenuCommunity.classList.add('animate__animated', 'animate__fadeInRight');
-}
-function showSubMenuHiddenSubcultural(){
-  listHiddenMenucultural.classList.toggle('hidden')
-  listHiddenMenucultural.classList.add('animate__animated', 'animate__fadeInRight');
-}
-function showSubMenuHiddenSubcomunities(){
-  listHiddenMenucommunities.classList.toggle('hidden')
-  listHiddenMenucommunities.classList.add('animate__animated', 'animate__fadeInRight');
-}
-
-iconHiddenMenu.addEventListener('click', showSubMenuHidden)
-iconHiddenMenuSubcoffee.addEventListener('click', showSubMenuHiddenSubcoffee)
-iconHiddenMenuSubFreeze.addEventListener('click', showSubMenuHiddenSubFreeze)
-iconHiddenMenuSubTea.addEventListener('click', showSubMenuHiddenSubTea)
-iconHiddenMenuSubbakery.addEventListener('click', showSubMenuHiddenSubBakery)
-iconHiddenMenuSuborder.addEventListener('click', showSubMenuHiddenSuborder)
-iconHiddenMenuSubnews.addEventListener('click', showSubMenuHiddenSubnews)
-iconHiddenMenuSubgobackme.addEventListener('click', showSubMenuHiddenSubgobackme)
-iconHiddenMenuSubCommunity.addEventListener('click', showSubMenuHiddenSubCommunity)
-iconHiddenMenuSubcultural.addEventListener('click', showSubMenuHiddenSubcultural)
-iconHiddenMenuSubcommunities.addEventListener('click', showSubMenuHiddenSubcomunities)
-
-
-
+listSubMenu.forEach((item) => {
+  item.addEventListener(
+    "click",
+    showSubMenu(item.parentNode.nextSibling.nextElementSibling)
+  );
+});
 
 /* menu order */
-const listSizes = document.querySelectorAll('.contentbox__size--item')
-const extPrice = document.getElementById('ext__price')
-  var liS = document.querySelector('.li1');
-  var liM = document.querySelector('.li2');
-  var liL = document.querySelector('.li3');
-  liS.onclick = function () {
-    liS.classList.toggle('current')
-    extPrice.innerHTML='49,000 VND'
-    liM.classList.remove('current')
-    liL.classList.remove('current')
-}
-  liM.onclick = function () {
-    liM.classList.toggle('current')
-    extPrice.innerHTML='59,000 VND'
-    liS.classList.remove('current')
-    liL.classList.remove('current')
-
-}
-  liL.onclick = function () {
-    liL.classList.toggle('current')
-    extPrice.innerHTML='69,000 VND'
-    liS.classList.remove('current')
-    liM.classList.remove('current')
-}
-  
+const listSizes = document.querySelectorAll(".contentbox__size--item");
+const extPrice = document.getElementById("ext__price");
+var liS = document.querySelector(".li1");
+var liM = document.querySelector(".li2");
+var liL = document.querySelector(".li3");
+liS.onclick = function () {
+  liS.classList.toggle("current");
+  extPrice.innerHTML = "49,000 VND";
+  liM.classList.remove("current");
+  liL.classList.remove("current");
+};
+liM.onclick = function () {
+  liM.classList.toggle("current");
+  extPrice.innerHTML = "59,000 VND";
+  liS.classList.remove("current");
+  liL.classList.remove("current");
+};
+liL.onclick = function () {
+  liL.classList.toggle("current");
+  extPrice.innerHTML = "69,000 VND";
+  liS.classList.remove("current");
+  liM.classList.remove("current");
+};
