af99a64d59a10cb96ce57b8433ae8a3f64c31993
le minh tu <leminhtu0908dtd@gmail.com>
2021-11-22 20:34:00 +0700

    update code

---
+++ currentLogin.php
@@ -0,0 +1,18 @@
+<div class="user-info">
+            <a class="user-info__link" href="">
+              <i class="far fa-user user-info__link--icon"></i>
+              <span class="user-info__link--name"><?php echo $_POST['email_login']; ?></span>
+              <i class="fas fa-sort-down user-info__link--icon"></i>
+            </a>
+            <div class="subuser-info">
+              <a class="subuser-info__link" href="./userInfo.php">
+                <p>Đơn hàng của tôi</p>
+              </a>
+              <a class="subuser-info__link" href="./userAccount.php">
+                <p>Tài khoản của tôi</p>
+              </a>
+              <a class="subuser-info__link" href="./userAccount.php">
+                <p>Đăng xuất</p>
+              </a>
+            </div>
+          </div>
