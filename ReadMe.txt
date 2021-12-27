d9c25dee16bf305a9ff99db646d458fbad47fc43
KhangNT1105 <KhangNT1105>
2021-12-27 21:53:40 +0700

    design structure databse

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+domain: http://ecomm.x10.mx/
+link github: https://github.com/KhangNT1105/ecommerce-proj
---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+define('HOST', 'localhost:3307');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DATABASE_NAME', 'ecommerce');
+
+define('CURRENCY', '$');
+define('IMG_STORE','images/')
+?>
